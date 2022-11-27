095b73216def1ad2aeb777639904e8643825fbe6
jmRiveral <60269766+jmRiveral@users.noreply.github.com>
2022-11-27 16:29:34 -0500

    sol recursiva

--- problemaP3.py
+++ problemaP3.py
@@ -1,15 +1,30 @@
 import sys
 import numpy as np
 
-def saltoDiv(m,k):
-    distancias =np.zeros(shape=(m+1,m+1))
-#crea mat de dist
-    for i in range(0,m+1):
-        for j in range(i,m+1):
-            dist=j-i
-            distancias[i][j]=dist
-        i+=1
-    print(distancias)
+def saltoDivRecursivo(m,ini,k,count):
+    aux=[] 
+    l=True
+    for i in range(ini+1,m+1):
+        mov=i-ini
+        
+     
+           
+        if mov%k==0 :
+            aux.append(i)
+          
+            
+    
+    for e in aux:
+        
+        if e==m:
+            print(aux)
+            print(e)
+            count+=1
+        elif (e+k+1)<=m:
+            count+= saltoDivRecursivo(m,e, k+1, 0)
+            
+    return count    
+  
   
 def main():
     linea = sys.stdin.readline()
@@ -22,5 +37,6 @@ def main():
         m=int(datastr[0])
         k=int(datastr[1])
         linea=sys.stdin.readline()
-        saltoDiv(m,k)
+        total =0
+        print(saltoDivRecursivo(m,0, k, total))
 main()
