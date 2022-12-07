9b30095c61eebbb94addb73433dde92fb9ab6b48
jmRiveral <60269766+jmRiveral@users.noreply.github.com>
2022-12-07 12:16:47 -0500

    the end

--- problemaP3.py
+++ problemaP3.py
@@ -36,7 +36,8 @@ def solSeguroDP(m,k):
     rep1={}
     rep2={}
     #inicializa aux
-    for i in range(1,m+1):
+    #rep indica cuantas manera se pueden llegar a dicho numero en la ruta existente
+    for i in range(1,m+1):#complejidad O(m/k)
         a=(k*i)
         if a<=m:
             aux1.append(a)
@@ -49,12 +50,13 @@ def solSeguroDP(m,k):
     
     
     s=1
-    while s<=m:
+    while s<=m:#es la iteracion sobre k, s es el numero de movimientos
+                #es en su peor caso teorico O(m/2)
         
         
-        for e in aux1:
+        for e in aux1:#esto en peor caso, teoricamente m/k
             act=e+k+s
-            while act<=m:
+            while act<=m:#esto es m/k peor caso
                 if act not in rep2:
                     
                     aux2.append(act)
@@ -72,7 +74,7 @@ def solSeguroDP(m,k):
             rep2[m]=rep1[m]
         
         
-        rep1=rep2.copy()
+        rep1=rep2.copy()#cada uno de estos es m/k peor caso
         rep2.clear()
         
         aux1=aux2.copy()
@@ -84,151 +86,15 @@ def solSeguroDP(m,k):
              
 
         
-        
+    #esto deberia ser O(m^3 /k^2)
        
     
     return rep1[m]
 
 
 
-
-def putamierda(m,k):
-    
-        
-    aux1={}
-    aux2={}
-    aux3={}
-    #inicializa aux
-    for i in range(1,m+1):
-        a=(k*i)%998244353
-        if a<=m:
-            aux1[a]=1
-            aux3[a]=1
-    
-    for i in range(1,m):#iterador numero movimentos
-        if len(aux1)==0:
-            break
-        act=0
-        for elem in aux1:#iterar sobre lista 1
-            act+= (k+i)
-            print(aux3)
-            sig=act+elem
-            if sig==m:
-                aux3[m]+=aux1[elem]
-            if sig in aux1:
-                aux3[sig]+=aux1[elem]
-            else: 
-                aux2[sig]=1
-                aux3[sig]=1
-        print(aux2)
-        aux1=aux2.copy()
-        aux2.clear()
-            
                 
-        
-        
-            
    
-    return aux3[m]
-
-
-def dicSol(m,k):
-    
-    contador=0
-    aux1={}
-    aux2={}
-    #inicializa aux
-    for i in range(1,m+1):
-        a=(k*i)
-        if a<=m:
-            aux1[a]=1
-    
-   
-    fc=0
-    for i in range(m):
-        k+=1
-       
-        #print("len aux1: ",len(aux1))
-        
-        if len(aux1)==0:
-        
-            break
-        aux3=list(aux1.keys())[:]
-        for j in aux3:
-            factor=1
-            sig=(factor*k) +j
-            
-            while  sig<=m :
-            
-                if sig in aux1:
-                    aux1[sig]+=aux1[j]
-                    aux1[j]=0
-                    if aux1[sig]==0:
-                        aux1[sig]=1
-                else:
-                    aux1[sig]=1
-                factor+=1
-                sig=((factor*k) +j)
-        print(aux1)
-        
-        
-       
-    return aux1[m]
-
-def test(m,k):
-    contador=0
-    aux1={}
-    #inicializa aux
-    for i in range(1,m+1):
-        a=(k*i)
-        if a<=m:
-            aux1[a]={0:0}
-    
-   
-    fc=0
-    for i in range(m):#crea arbol
-        k+=1
-       
-        #print("len aux1: ",len(aux1))
-        
-        if len(aux1)==0:
-        
-            break
-        aux3=list(aux1.keys())[:]
-        for j in aux3:
-            factor=1
-            sig=(factor*k) +j
-            
-            while  sig<=m :
-            
-                if sig in aux1:
-                    if j not in aux1[sig]:
-                        aux1[sig][j]=0
-                   
-                   
-                else:
-                    aux1[sig]={}
-                    aux1[sig][j]=0
-                factor+=1
-                sig=((factor*k) +j)
-        print(aux1)
-    
-    
-       
-    return pls(aux1,0,m)
-
-def pls(tredic,paths,nodo):
-    
-    if nodo==0:
-        return 1
-    
-    
-    for hijo in tredic[nodo]:
-        paths+=pls(tredic,paths,hijo)
-
-    return paths
-    
-
 
 def main():
     linea = sys.stdin.readline()
@@ -244,11 +110,8 @@ def main():
         total =0
         start_time = time.time()
         print(solSeguroDP(m, k))
-        #print(m,":",dicSol(m,k))
-        #print("--- %s seconds ---" % (time.time() - start_time))
-        #print(test(m, k))
-        #print(saltoDivRecursivo(m,0, k, total))
+      
 
-start_time = time.time()
+
 main()
-print("--- %s seconds ---" % (time.time() - start_time))
+
